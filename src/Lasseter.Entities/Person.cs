128b5314ae0b0d1591360a68581e2722446fe50b
Matthew <joel.skinner.griffith@hotmail.com>
2012-09-10 16:20:17 +1000

    Person

---
+++ src/Lasseter.Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lasseter.Entities
+{
+    class Person
+    {
+        public String Name { get; set; }
+        public String Skills { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public String Other { get; set; }
+        public int PostCode { get; set; }
+    }
+
+
+}
